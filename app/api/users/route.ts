4ecc5aa3c3b0d37d6b76d7aa3dd6f1b29396e9e5
teethw <kazisadman01@gmail.com>
2025-02-14 00:34:39 +0600

    feat:add end point to create new user in db
    - Implemented a new POST endpoint pi/users to create new user
    
    - Successfully handle insertation of user into the database

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import connect from "@/app/lib/db";
 import User from "@/app/lib/model/user";
 import { NextResponse } from "next/server";
@@ -7,9 +8,23 @@ export const GET = async () => {
     await connect();
     const users = await User.find();
     return new NextResponse(JSON.stringify(users), { status: 200 });
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error:any) {
+  } catch (error: any) {
     return new NextResponse("Error while fetching users" + error.message, {
+      status: 500,
+    });
+  }
+};
+
+export const POST = async (req: Request) => {
+  try {
+    const body = await req.json();
+    await connect();
+    const result = await User.create(body);
+    return new NextResponse(
+      JSON.stringify({ message: "User is created", user: result })
+    );
+  } catch (error:any) {
+    return new NextResponse("Error while creating user" + error.message, {
       status: 500,
     });
   }
